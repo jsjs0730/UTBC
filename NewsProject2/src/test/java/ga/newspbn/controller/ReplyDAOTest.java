db3f7bd317fcea970c2cacb943fa35a092099e29
Williams789 <bakamono789@yahoo.co.jp>
2018-01-02 03:27:04 +0900

    dao.getMaxDepth 테스트

--- NewsProject2/src/test/java/ga/newspbn/controller/ReplyDAOTest.java
+++ NewsProject2/src/test/java/ga/newspbn/controller/ReplyDAOTest.java
@@ -28,10 +28,16 @@ public class ReplyDAOTest {
 	
 	private static Logger logger = LoggerFactory.getLogger(ReplyDAOTest.class);
 	@Test
-	public void testMethod() throws Exception{
+	public void testGetMaxDepth() throws Exception{
 		ReplyVO vo = new ReplyVO();
-		String oldDepth = "5@53@45456";
-		vo.setDepth(oldDepth);
+		String oldDepth = "38";
+		String tempDepth = oldDepth + "@";
+		vo.setDepth(tempDepth);
+		int getMaxDepth = (Integer)dao.getMaxDepth(vo);
+		logger.debug("int getMaxDepth = (Integer)dao.getMaxDepth(vo);" + getMaxDepth);
+		//결과 :  oldDepth = "28"; 28@2 에 있는 2 를 반환 함
 		
+		String newDepth = tempDepth + ((int)getMaxDepth+1);
+		System.out.println("새로운 번호를 가진 getDepth : " + newDepth);
 	}
 }
