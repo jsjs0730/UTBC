e0dc9169c60bcb962ffef6719b5db560fb9919cd
Williams789 <bakamono789@yahoo.co.jp>
2018-01-01 13:51:20 +0900

    gitignore 적용 안되는듯

---
+++ gi.bat
@@ -0,0 +1,9 @@
+@ECHO OFF
+set argc=0
+for %%x in (%*) do Set /A argc+=1
+set IGNORE="ecilpse,java,maven,windows,java-web"
+IF %argc% EQU 0 (
+	curl https://www.gitignore.io/api/%IGNORE%
+) ELSE (
+	curl https://www.gitignore.io/api/%1
+)
