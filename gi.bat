bbae35da17ade7a35bb2dd8b787d25a3d3ff1e93
Williams789 <bakamono789@yahoo.co.jp>
2018-01-03 10:37:24 +0900

    eclipse 오타수정

--- gi.bat
+++ gi.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 set argc=0
 for %%x in (%*) do Set /A argc+=1
-set IGNORE="ecilpse,java,maven,windows,java-web"
+set IGNORE="eclipse,java,maven,windows,java-web"
 IF %argc% EQU 0 (
 	curl https://www.gitignore.io/api/%IGNORE%
 ) ELSE (
